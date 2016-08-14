a2138edc3f873b50ad37f634123d528a1d2888a8
Soso Tughushi <mryemama@gmail.com>
2016-08-15 02:53:01 +0400

    Butified filters in AjaxDemoDataTable

--- TomTom.DataTable/TomTom.DataTable/Filters/DataTableFilterDisplay.cs
+++ TomTom.DataTable/TomTom.DataTable/Filters/DataTableFilterDisplay.cs
@@ -11,10 +11,14 @@ namespace TomTom.DataTable.Razor
 
     public class DataGridFilterDisplay
     {
+        public MvcHtmlString SubmitButton { get; private set; }
+        public MvcHtmlString ItemsPerPage { get; private set; }
 
         private readonly List<Tuple<FilterOption, MvcHtmlString>> _filters;
-        public DataGridFilterDisplay(List<Tuple<FilterOption, MvcHtmlString>> filters)
+        public DataGridFilterDisplay(List<Tuple<FilterOption, MvcHtmlString>> filters, MvcHtmlString submitButton, MvcHtmlString itemsPerPage)
         {
+            SubmitButton = submitButton;
+            ItemsPerPage = itemsPerPage;
             _filters = filters;
         }
 
